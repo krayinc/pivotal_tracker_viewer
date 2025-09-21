0c6d8b700995298587e6a182a9059ea6b5e8eb41
Takatoshi MORIYAMA <moriyama.takatoshi@gmail.com>
2025-09-21 12:40:04 +0900

    Pivotalリンクの内製化と詳細テキストのURL置換

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -2,6 +2,7 @@ require "uri"
 
 module StoriesHelper
   PIVOTAL_TRACKER_PATH_REGEX = %r{/(?:story|stories)/show/(\d+)}.freeze
+  PIVOTAL_TRACKER_URL_REGEX = %r{https?://(?:www\.)?pivotaltracker\.com/(?:story|stories)/show/(\d+)}i.freeze
 
   def local_story_url(story)
     tracker_id = story&.tracker_id.presence || tracker_id_from_url(story&.url)
@@ -16,5 +17,18 @@ module StoriesHelper
     URI.parse(url).path[PIVOTAL_TRACKER_PATH_REGEX, 1]
   rescue URI::InvalidURIError
     nil
+  end
+
+  def rewrite_pivotal_story_urls(text)
+    return text if text.blank?
+
+    text.to_s.gsub(PIVOTAL_TRACKER_URL_REGEX) do
+      tracker_id = Regexp.last_match(1)
+      story_tracker_url(tracker_id)
+    end
+  end
+
+  def render_story_markdown(text)
+    render_markdown(rewrite_pivotal_story_urls(text))
   end
 end
